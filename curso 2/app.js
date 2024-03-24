7cd4213c8695c627d143d1d6c61a47fc8e270c05
Julio César <julio06.sp@gmail.com>
2024-03-24 14:50:00 -0300

    Atualização curso 2

---
+++ curso 2/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do Número Secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha entre um número de 1 e 10';
+
+function verificarChute() {
+    console.log('O botão foi clicade!')
+}
