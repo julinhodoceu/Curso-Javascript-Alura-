8f48656d2791e1054d3b210a2f3df52774aeaf06
Julio César <julio06.sp@gmail.com>
2024-03-23 23:12:12 -0300

    atualização do projeto

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-alert('Bem vindo ao jogo de adivinhar o número')
+/* alert('Bem vindo ao jogo de adivinhar o número');
 
-let numeroDaSorte = 3
+let numeroDaSorte = 3;
+let chute;
+let quantidadeTentativas = 1;
 
 function chutarNumero() {
 
-    let chute = prompt('Insira um número entre 1 e 10')
+    while (chute != numeroDaSorte) {
 
-if (chute == numeroDaSorte) {
-    alert('parabéns moreh, você descobriu o secret number, que é ' + numeroDaSorte)
-} else {
-    alert ('poxa, que penah! tente de novo')
-    chutarNumero()
-}
+        chute = parseInt(prompt('Insira um número entre 1 e 10'));
+
+        if (chute == numeroDaSorte && quantidadeTentativas != 1) {
+            alert('parabéns moreh, você descobriu o secret number, que é ' + numeroDaSorte + ', com apenas ' + quantidadeTentativas + ' tentativas');
+
+        } else if (chute == numeroDaSorte && quantidadeTentativas === 1) {
+            alert('parabéns moreh, você descobriu o secret number, que é ' + numeroDaSorte + ', com apenas ' + quantidadeTentativas + ' tentativa');
+
+        } else {
+            quantidadeTentativas++;
+            if (chute > numeroDaSorte) {
+                alert('O número da sorte é menor do que ' + chute + '. Essa é sua ' + quantidadeTentativas + 'º Tentativa');
+            } else {
+                alert('O número da sorte é maior do que ' + chute + '. Essa é sua ' + quantidadeTentativas + 'º Tentativa');
+            }
+
+        }
+    }
 }
 
-chutarNumero()
+chutarNumero();
+
+*/
+
+let contador = 0;
+
+while (contador < 11) {
+    console.log(contador)
+    contador++;
+}
