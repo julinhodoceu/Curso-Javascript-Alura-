d08f2590563c4b2d40e905c8759078e30607fbfe
Julio César <julio06.sp@gmail.com>
2024-04-07 00:10:34 -0300

    finalização do projeto

--- curso 2/app.js
+++ curso 2/app.js
@@ -1,9 +1,73 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Número Secreto';
+let listaDeNumeros = [];
+let numeroSecreto = gerarNumeroAleatorio();
+let quantidadeDeTentativas = 1;
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha entre um número de 1 e 10';
+function substituirTextos(tag,texto) {
+    let campos = document.querySelector(tag);
+    campos.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+
+}
+
+function exibirMensagemInicial() {
+
+substituirTextos('h1','Jogo do número da sorte');
+substituirTextos('p','Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
+console.log(numeroSecreto)
 
 function verificarChute() {
-    console.log('O botão foi clicade!')
+    let chute = document.querySelector('input').value;
+    let palavraTentativa = quantidadeDeTentativas == 1 ? 'tentativa' : 'tentativas';
+    let mensagemTentativas = 'Paraboins, você descobriu o número! Que é ' + numeroSecreto + ' Com apenas ' + quantidadeDeTentativas + ' ' + palavraTentativa;
+
+    if (chute == numeroSecreto) {
+        substituirTextos ('h1', 'VOCÊ ACERTOU!');
+        substituirTextos ('p',mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        document.querySelector('input').disabled = true;
+    } else {
+        if (numeroSecreto > chute) {
+            substituirTextos ('p', 'O número é maior do que ' + chute);
+        } else {
+            substituirTextos ('p', 'O número é menor do que ' + chute);
+        }
+        quantidadeDeTentativas++;
+        limparCampo();
+    }
 }
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+};
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * 10 + 1);
+    let quantidadeDeElementosNaLista = listaDeNumeros.length;
+
+    if (quantidadeDeElementosNaLista == 10) {
+        listaDeNumeros = [];
+    }
+
+    if (listaDeNumeros.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio()
+    } else {
+        listaDeNumeros.push(numeroEscolhido);
+        console.log(listaDeNumeros)
+        return numeroEscolhido;
+    }
+};
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    quantidadeDeTentativas = 1;
+    console.log(numeroSecreto);
+    exibirMensagemInicial();
+    document.querySelector('input').disabled = false;
+    document.querySelector('#reiniciar').disabled = true;
+}
+
---
+++ curso 2/exercicios curso 2.js
@@ -0,0 +1,62 @@
+/*
+function mensagem() {
+    console.log('Olá, mundo!')
+}
+
+mensagem();
+
+function nomeSelecionado(nome) {
+    alert(nome)
+}
+
+nomeSelecionado('Julinhor');
+
+function dobroDoNumero (numero){
+    alert(numero * 2)
+}
+
+dobroDoNumero(40);
+
+function mediaTresNumeros (numero1,numero2,numero3) {
+    let media = ((numero1 + numero2 + numero3) / 3) 
+    alert('a média de ' + numero1 + ' + ' + numero2 + ' + ' + numero3 + ' é de ' + media)
+}
+
+mediaTresNumeros(24,22,50)
+
+function numeroMaior (num1,num2) {
+    if (num1 > num2) {
+        alert(num1 + ' é maior do que ' + num2)
+    } else {
+        alert(num2 + ' é maior do que ' + num1)
+    }
+}
+
+numeroMaior(38,20)
+
+function elePorEle(number1) {
+    alert(number1 * number1)
+}
+
+elePorEle (10)
+
+*/
+
+function calculoIMC(peso,altura) {
+let imc = peso / (altura * altura)
+alert('seu imc é de ' + imc);
+}
+
+let seuPeso = prompt('Qual seu pesoa?')
+let suaAltura = prompt('Qual sua alturoa?')
+
+calculoIMC(seuPeso,suaAltura);
+
+function dolarParaReal (dolar) {
+let real = dolar * 4.80;
+alert('Você tem ' + real + ' reais')
+}
+
+dolarParaReal(2)
+
+
